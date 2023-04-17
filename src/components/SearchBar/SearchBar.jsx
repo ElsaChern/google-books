66665f7ab9f15b40d1a2785f66265cf4e7c1ed5a
Elizabeth <elizaveta.chernosheikina@yandex.ru>
2023-04-17 18:08:23 +0300

    add books api

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,25 +1,25 @@
+/* eslint-disable no-console */
 import { Input, SearchDiv, SelectOption, Text } from "./styled";
 import { categorySelectOptions, sortSelectOptions } from "./constants";
+import fetchBooks from "../../api/fetchBooks";
 
 const SearchBar = () => {
+  const res = fetchBooks("JS", "computers", "relevance");
+  console.log(res);
   return (
-    <>
-      <SearchDiv>
-        <Input
-          placeholder="Search"
-        />
-        <Text>Categories:</Text>
-        <SelectOption
-          defaultValue={categorySelectOptions[0]}
-          options={categorySelectOptions}
-        />
-        <Text>Sorting By:</Text>
-        <SelectOption
-          defaultValue={sortSelectOptions[0]}
-          options={sortSelectOptions}
-        />
-      </SearchDiv>
-    </>
+    <SearchDiv>
+      <Input placeholder="Search" />
+      <Text>Categories:</Text>
+      <SelectOption
+        defaultValue={categorySelectOptions[0]}
+        options={categorySelectOptions}
+      />
+      <Text>Sorting By:</Text>
+      <SelectOption
+        defaultValue={sortSelectOptions[0]}
+        options={sortSelectOptions}
+      />
+    </SearchDiv>
   );
 };
 
